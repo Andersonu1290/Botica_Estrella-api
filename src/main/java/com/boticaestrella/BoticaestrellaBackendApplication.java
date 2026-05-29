d68d8b9ad369867e7e9aac68df13f9125f615ca8
Andersonu1290 <andersonurrutiamoreyra@gmail.com>
2026-05-29 15:09:49 -0500

    Primer commit

---
+++ src/main/java/com/boticaestrella/BoticaestrellaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.boticaestrella;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoticaestrellaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoticaestrellaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/boticaestrella/BoticaestrellaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boticaestrella;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoticaestrellaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
